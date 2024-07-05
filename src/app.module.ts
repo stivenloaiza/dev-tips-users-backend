b1016231aae43a2b2b85d1662187c4c4073994e3
holas1356 <ruthbustamante075@gmail.com>
2024-07-05 12:31:53 -0500

    entity of users and companies is created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PersistanceModuleModule } from './modules/persistance-module.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PersistanceModuleModule],
+  imports: [PersistanceModuleModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,46 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({ timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' } })
+export class User extends Document {
+    @Prop({ required: true })
+    apiKey: string;
+
+    @Prop({ required: true })
+    name: string;
+
+    @Prop({ required: true, unique: true })
+    email: string;
+
+    @Prop({ required: true })
+    phone: string;
+
+    /* @Prop({ required: true, enum: UserRole })
+    role: UserRole; */
+
+    /* @Prop({ required: function() { return this.role === UserRole.COMPANY; } })
+    managerName?: string;
+
+    @Prop({ required: function() { return this.role === UserRole.COMPANY; } })
+    managerEmail?: string;
+
+    @Prop({ required: function() { return this.role === UserRole.COMPANY; } })
+    managerPhone?: string; */
+
+    /* @Prop({ required: true, enum: SubscriptionType })
+    subscriptions: SubscriptionType; */
+
+    @Prop({ default: null })
+    deletedAt?: Date;
+
+    @Prop({ default: null })
+    createdBy?: string;
+
+    @Prop({ default: null })
+    updatedBy?: string;
+
+    @Prop({ default: null })
+    deletedBy?: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
