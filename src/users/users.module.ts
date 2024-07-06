eec7471220adbc761763b375ad14f3d00919a735
holas1356 <ruthbustamante075@gmail.com>
2024-07-06 16:03:59 -0500

    the crud of the user service is created

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './entities/user.entity';
+import {  UsersService } from './users.service';
 
 @Module({
   imports: [
