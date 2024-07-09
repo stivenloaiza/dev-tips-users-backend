1bb9fc75473a41ad5428d60c4f651201e3fcd8a9
holas1356 <ruthbustamante075@gmail.com>
2024-07-09 18:32:56 -0500

    modified

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,22 +11,18 @@ export class UsersController {
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
-
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
-
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
-
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
-
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
