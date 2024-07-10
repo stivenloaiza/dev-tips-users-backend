18d4bd810ffa62a1a412b1b33c47fe89f0f234f8
holas1356 <ruthbustamante075@gmail.com>
2024-07-10 10:36:26 -0500

    modified

--- src/iframes/iframes.service.ts
+++ src/iframes/iframes.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateIframeDto } from './dto/create-iframe.dto';
