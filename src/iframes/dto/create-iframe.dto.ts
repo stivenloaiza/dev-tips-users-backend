318beb696f15272149ed7b77f84bb1d590db367c
holas1356 <ruthbustamante075@gmail.com>
2024-07-10 10:24:53 -0500

    iframe entity and data are created to create and update the subscription

--- src/iframes/dto/create-iframe.dto.ts
+++ src/iframes/dto/create-iframe.dto.ts
@@ -1,1 +1,37 @@
-export class CreateIframeDto {}
+import { language, programmingLanguage, senority } from 'src/libs/enums';
+import { IsString, IsNotEmpty, IsEnum } from 'class-validator';
+import { Types } from 'mongoose';
+
+
+export class CreateIframeDto {
+    @IsNotEmpty()
+    userId: Types.ObjectId;
+
+    @IsString()
+    @IsNotEmpty()
+    communication: string;
+
+    @IsEnum(senority)
+    @IsNotEmpty()
+    seniority: senority;
+
+    @IsEnum(programmingLanguage)
+    @IsNotEmpty()
+    programmingLanguage: programmingLanguage;
+
+    @IsString()
+    @IsNotEmpty()
+    domains: string;
+
+    @IsString()
+    @IsNotEmpty()
+    color: string;
+
+    @IsString()
+    @IsNotEmpty()
+    typography: string;
+
+    @IsEnum(language)
+    @IsNotEmpty()
+    language: language;
+}
