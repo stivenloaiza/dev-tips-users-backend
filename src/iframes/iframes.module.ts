5a2a823a0a0fceac818e2694d93f82321e4d106d
holas1356 <ruthbustamante075@gmail.com>
2024-07-10 10:41:34 -0500

    the entity is included in the .module

--- src/iframes/iframes.module.ts
+++ src/iframes/iframes.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { IframesService } from './iframes.service';
 import { IframesController } from './iframes.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { IframeSchema, IframeSuscription } from './entities/iframe.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: IframeSuscription.name, schema: IframeSchema }])
+],
   controllers: [IframesController],
   providers: [IframesService],
 })
