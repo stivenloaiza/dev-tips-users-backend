ded8b990337ac774e1f869830a20c53a1b1a02f4
Thomasrr29 <thomasrr29@gmail.com>
2024-07-11 08:58:20 -0500

    email entity

--- src/module/users/users.module.ts
+++ src/module/users/users.module.ts
@@ -1,12 +1,1 @@
-import { Module } from '@nestjs/common';
-import { UserSchema } from './entities/user.entity';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
-import { MongooseModule } from '@nestjs/mongoose';
 
-@Module({
-  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
-  controllers: [UsersController],
-  providers: [UsersService],
-})
-export class UsersModule {}
