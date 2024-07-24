8cc5cd5f95d38230cd8126bb89d83c627fe823c3
Samuel Quintero Solís <samuelquisol@gmail.com>
2024-07-23 20:33:32 -0500

    Swagger Implemented In Users Modules

--- src/module/users/dto/create-user.dto.ts
+++ src/module/users/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, getSchemaPath } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import {
   IsArray,
@@ -15,9 +15,25 @@ import { CreateTvDto } from 'src/module/tvs/dto/create-tv.dto';
 import { CreateEmailDto } from 'src/module/email/dto/create-email.dto';
 
 export class SubscriptionDto {
+  @ApiProperty({
+    enum: SubscriptionType,
+    enumName: 'SubscriptionType',
+    description:
+      'The type of subscription, which can be one of the predefined values in SubscriptionType enum.',
+  })
   @IsEnum(SubscriptionType)
   type: SubscriptionType;
 
+  @ApiProperty({
+    description:
+      'The data related to the subscription, which can be of different types based on the subscription type.',
+    oneOf: [
+      { $ref: getSchemaPath(CreateBotsSubscriptionDto) },
+      { $ref: getSchemaPath(CreateTvDto) },
+      { $ref: getSchemaPath(CreateIframeDto) },
+      { $ref: getSchemaPath(CreateEmailDto) },
+    ],
+  })
   @ValidateNested()
   @Type(() => Object)
   data:
@@ -28,55 +44,104 @@ export class SubscriptionDto {
 }
 
 export class CreateUserDto {
-  @ApiProperty()
+  @ApiProperty({
+    example: '12345-abcdef-67890-ghijk',
+    description:
+      'The API key assigned to the user for authentication purposes.',
+  })
   @IsString()
   apiKey: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'John Doe',
+    description: 'The full name of the user.',
+  })
   @IsString()
   name: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'john.doe@example.com',
+    description: 'The email address of the user.',
+  })
   @IsEmail()
   email: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: '+1234567890',
+    description: 'The phone number of the user, including the country code.',
+  })
   @IsString()
   phone: string;
 
-  @ApiProperty({ enum: UserRole, enumName: 'UserRole' })
+  @ApiProperty({
+    enum: UserRole,
+    enumName: 'UserRole',
+    description:
+      'The role assigned to the user, which determines their permissions and access level.',
+  })
   @IsEnum(UserRole)
   role: UserRole;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'Jane Doe',
+    description: "The full name of the user's manager.",
+    required: false,
+  })
   @IsOptional()
   @IsString()
   managerName?: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'jane.doe@example.com',
+    description: "The email address of the user's manager.",
+    required: false,
+  })
   @IsOptional()
   @IsEmail()
   managerEmail?: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: '+0987654321',
+    description:
+      "The phone number of the user's manager, including the country code.",
+    required: false,
+  })
   @IsOptional()
   @IsString()
   managerPhone?: string;
 
+  @ApiProperty({
+    type: [SubscriptionDto],
+    description: 'A list of subscriptions associated with the user.',
+  })
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => SubscriptionDto)
   subscriptions: SubscriptionDto[];
 
-  @ApiProperty({ type: Date, default: null })
+  @ApiProperty({
+    type: Date,
+    default: null,
+    description:
+      'The date and time when the user was deleted. Null if the user is not deleted.',
+  })
   deletedAt?: Date;
 
-  @ApiProperty({ default: null })
+  @ApiProperty({
+    default: null,
+    description: 'The user who created this user entry.',
+  })
   createdBy?: string;
 
-  @ApiProperty({ default: null })
+  @ApiProperty({
+    default: null,
+    description: 'The user who last updated this user entry.',
+  })
   updatedBy?: string;
 
-  @ApiProperty({ default: null })
+  @ApiProperty({
+    default: null,
+    description: 'The user who deleted this user entry.',
+  })
   deletedBy?: string;
 }
--- src/module/users/users.controller.ts
+++ src/module/users/users.controller.ts
@@ -10,29 +10,95 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user-dto';
 import { User } from './entities/user.entity';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiParam,
+  ApiBody,
+} from '@nestjs/swagger';
 
-@ApiTags('users')
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('/create')
-  async create(@Body() createUserDto: any): Promise<any> {
+  @ApiOperation({ summary: 'Create a new user' })
+  @ApiBody({
+    type: CreateUserDto,
+    description: 'Data to create a new user',
+    examples: {
+      example1: {
+        summary: 'Complete example',
+        description: 'A full example of the data required to create a user',
+        value: {
+          apiKey: '12345-abcdef-67890-ghijk',
+          name: 'John Doe',
+          email: 'john.doe@example.com',
+          phone: '+1234567890',
+          role: 'person',
+          managerName: 'Jane Doe',
+          managerEmail: 'jane.doe@example.com',
+          managerPhone: '+0987654321',
+          subscriptions: [
+            {
+              type: 'iframe',
+              data: {
+                apikey: '67890-ghijk-12345-abcdef',
+                userId: '60c72b2f9b1e8e2f88d9b123',
+                type: 'Basic',
+                level: 'junior',
+                technology: 'JavaScript',
+                lang: 'english',
+                iframe:
+                  '<iframe src="https://example.com/iframe" width="600" height="400"></iframe>',
+              },
+            },
+          ],
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'User created successfully.',
+    type: User,
+  })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
     try {
       return await this.usersService.create(createUserDto);
     } catch (error) {
       if (error instanceof BadRequestException) {
         throw error;
       } else {
-        throw new BadRequestException('Error creating user pepeep.');
+        throw new BadRequestException('Error creating user.');
       }
     }
   }
 
   @Get('/:page/:limit')
+  @ApiOperation({ summary: 'Get a paginated list of users' })
+  @ApiParam({
+    name: 'page',
+    type: Number,
+    description: 'Page number for pagination',
+  })
+  @ApiParam({
+    name: 'limit',
+    type: Number,
+    description: 'Number of items per page',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Paginated list of users returned successfully.',
+    type: [User],
+  })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
   async findAll(
     @Param('page') page: number,
     @Param('limit') limit: number,
@@ -41,6 +107,14 @@ export class UsersController {
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get details of a specific user by ID' })
+  @ApiParam({ name: 'id', type: String, description: 'ID of the user' })
+  @ApiResponse({
+    status: 200,
+    description: 'User details returned successfully.',
+    type: User,
+  })
+  @ApiResponse({ status: 404, description: 'User not found.' })
   async findOne(@Param('id') id: string): Promise<User> {
     const user = await this.usersService.findOne(id);
     if (!user) {
@@ -50,6 +124,14 @@ export class UsersController {
   }
 
   @Get('/findByEmail/:email')
+  @ApiOperation({ summary: 'Get user details by email' })
+  @ApiParam({ name: 'email', type: String, description: 'Email of the user' })
+  @ApiResponse({
+    status: 200,
+    description: 'User details returned successfully.',
+    type: User,
+  })
+  @ApiResponse({ status: 404, description: 'User not found.' })
   async findByEmail(@Param('email') email: string) {
     const user = await this.usersService.findUserByEmail(email);
     if (!user) {
@@ -62,6 +144,29 @@ export class UsersController {
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a specific user by ID' })
+  @ApiParam({ name: 'id', type: String, description: 'ID of the user' })
+  @ApiBody({
+    type: UpdateUserDto,
+    description: 'Data to update the user',
+    examples: {
+      example1: {
+        summary: 'Partial update example',
+        description: 'An example of partially updating a user',
+        value: {
+          name: 'Updated Name',
+          email: 'updated.email@example.com',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'User updated successfully.',
+    type: User,
+  })
+  @ApiResponse({ status: 404, description: 'User not found.' })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
   async update(
     @Param('id') id: string,
     @Body() updateUserDto: UpdateUserDto,
@@ -82,6 +187,15 @@ export class UsersController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a specific user by ID' })
+  @ApiParam({ name: 'id', type: String, description: 'ID of the user' })
+  @ApiResponse({
+    status: 200,
+    description: 'User deleted successfully.',
+    type: User,
+  })
+  @ApiResponse({ status: 404, description: 'User not found.' })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
   async remove(@Param('id') id: string): Promise<User> {
     try {
       const userId = 'admin';
