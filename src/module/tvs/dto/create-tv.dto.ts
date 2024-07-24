7828404a2783af3384072bb1f74be2ae8efb1036
Samuel Quintero Solís <samuelquisol@gmail.com>
2024-07-24 08:32:00 -0500

    Swagger Implemented In tvs Module

--- src/module/tvs/dto/create-tv.dto.ts
+++ src/module/tvs/dto/create-tv.dto.ts
@@ -3,27 +3,29 @@ import { IsString, IsEnum, IsNotEmpty } from 'class-validator';
 import { devLanguageType, languageType, seniorityType } from 'src/libs/enums';
 
 export class CreateTvDto {
+  @ApiProperty({ description: 'API key for the TV subscription' })
   @IsString()
   apikey: string;
 
+  @ApiProperty({ description: 'User ID associated with the TV subscription' })
   @IsString()
   userId: string;
 
-  @ApiProperty()
+  @ApiProperty({ description: 'Type of TV subscription' })
   @IsString()
   type: string;
 
-  @ApiProperty()
+  @ApiProperty({ description: 'Seniority level of the TV subscription' })
   @IsNotEmpty()
   @IsEnum(seniorityType)
   level: seniorityType;
 
-  @ApiProperty()
+  @ApiProperty({ description: 'Technology associated with the TV subscription' })
   @IsNotEmpty()
   @IsEnum(devLanguageType)
   technology: devLanguageType;
 
-  @ApiProperty()
+  @ApiProperty({ description: 'Language of the TV subscription' })
   @IsNotEmpty()
   @IsEnum(languageType)
   lang: languageType;
